f1fe139dcf367ddcc0b6ca63156c1baf3fa3a558
Sanusi Muhyideen <just4deen@yahoo.com>
2022-06-21 16:04:48 +0100

    create functionality  to at quote randomly

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-// fetch Anime Api list random
-async function start() {
-  const response = await fetch("https://animechan.vercel.app/api/random");
-  const data = await response.json();
-  console.log(data);
-  console.log(data.anime);
-  console.log(data.character);
-  console.log(data.quote);
+// Decleared Variable
+const btn = document.getElementById("btn_anime");
 
-  createQoute();
-}
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
 
-start();
+  // fetch Anime Api list random
+  async function start() {
+    const response = await fetch("https://animechan.vercel.app/api/random");
+    const data = await response.json();
+    console.log(data);
+    // console.log(data.anime);
+    // console.log(data.character);
+    // console.log(data.quote);
+
+    createQuote(data.quote, data.anime, data.character);
+  }
+
+  start();
+
+  console.log("hello");
+  function createQuote(quote, anime, chracter) {
+    document.querySelector(".qoute-box").innerHTML = `
+    <p class="qoute">${quote}</p>
+    <p class="anime">Anime ${anime}</p>
+    <p class="chracter">${chracter}</p>
+   
+   `;
+  }
+});
